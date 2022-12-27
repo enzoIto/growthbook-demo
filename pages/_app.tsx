4035391a98322273a4d39dd9efdf4dd14fc5b175
Massaki <massakivx@gmail.com>
2022-12-27 13:28:20 -0300

    Growthbook-poc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { GrowthBook, GrowthBookProvider } from "@growthbook/growthbook-react";
+import { useEffect } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const FEATURES_ENDPOINT = "https://cdn.growthbook.io/api/features/prod_wnqcb7sgyBXIrkonjQPNdIrNMosq5rfJNRBuTIWIE";
+
+// Create a GrowthBook instance
+const growthbook = new GrowthBook({
+  trackingCallback: (experiment, result) => {
+    console.log("Viewed Experiment", experiment, result);
+  },
+});
+
+export default function MyApp({ Component, pageProps, router }: any) {
+  // Refresh features and targeting attributes on navigation
+  useEffect(() => {
+    fetch(FEATURES_ENDPOINT)
+      .then((res) => res.json())
+      .then((json) => {
+        growthbook.setFeatures(json.features);
+      });
+
+    growthbook.setAttributes({
+      id: "123",
+      loggedIn: true,
+      deviceId: "abcdef123456",
+      employee: true,
+      company: "acme",
+      country: "US",
+      browser: navigator.userAgent,
+      url: router.pathname,
+    });
+  }, [router.pathname]);
+
+  return (
+    <GrowthBookProvider growthbook={growthbook}>
+      <Component {...pageProps} />
+    </GrowthBookProvider>
+  );
 }
